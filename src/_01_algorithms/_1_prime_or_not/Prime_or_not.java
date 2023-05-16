5159c1e1b041f78de503b99d1c84e5d365bef1db
echohunter <eddiejhunter1000000@gmail.com>
2023-05-15 17:00:51 -0700

    prime or not

---
+++ src/_01_algorithms/_1_prime_or_not/Prime_or_not.java
@@ -0,0 +1,61 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class Prime_or_not {
+
+
+public static void main(String[] args) {
+	
+String num = JOptionPane.showInputDialog("Choose a number");
+int number = Integer.parseInt(num);
+for(int i=2; i<number; i++) {
+	if (number%i==(0)) {
+		JOptionPane.showMessageDialog(null, "the number is not prime");
+		System.exit(0);
+	}
+	
+}
+JOptionPane.showMessageDialog(null, "the number is prime");	  
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
