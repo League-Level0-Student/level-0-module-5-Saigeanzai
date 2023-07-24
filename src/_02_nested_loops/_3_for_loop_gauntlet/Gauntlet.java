48438afded4676f2592f4ab12b59a7f3ff26278f
jereycui2008 <jeremycuid@gmail.com>
2023-07-24 16:57:11 -0700

    I did gauntlet

---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/Gauntlet.java
@@ -0,0 +1,40 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+public class Gauntlet {
+
+	public static void main(String [] args) {
+	
+		
+		for(int i=1; i<100; i++) {
+			if (i%2==(1)) {
+				System.out.println(i);
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
