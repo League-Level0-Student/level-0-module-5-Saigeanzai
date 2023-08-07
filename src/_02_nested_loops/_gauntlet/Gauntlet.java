5f314a78b17503e46f17f7e5f9b51b0333223d92
lvl0_module2 <zoey.lestyk@gmail.com>
2023-08-07 16:55:46 -0700

    I did Gauntlet and intro to methods.

---
+++ src/_02_nested_loops/_gauntlet/Gauntlet.java
@@ -0,0 +1,13 @@
+package _02_nested_loops._gauntlet;
+
+import javax.swing.JOptionPane;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
+public class Gauntlet {
+	public static void main(String[] args) {
+		for (int i = 0; i<13; ++i) {
+		System.out.println(" in " + (2023 - 12 +i) + " I was " + i + " years old");
+		}
+		
+	}
+}
---
+++ src/_03_method_writing/_1_intro_to_methods/Gauntlet2.java
@@ -0,0 +1,5 @@
+package _03_method_writing._1_intro_to_methods;
+
+public class Gauntlet2 {
+main 
+}
