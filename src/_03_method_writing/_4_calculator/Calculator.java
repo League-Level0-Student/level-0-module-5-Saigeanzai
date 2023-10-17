6c4119d8e319fa8377e35e00e8e7ef712f565195
jereycui2008 <jeremycuid@gmail.com>
2023-10-16 17:01:04 -0700

    I did happy pet and calculator.

--- src/_03_method_writing/_4_calculator/Calculator.java
+++ src/_03_method_writing/_4_calculator/Calculator.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 
 public class Calculator {
 
-	//GOAL: MAKE A CALCULATOR
+	// GOAL: MAKE A CALCULATOR
 	public static void main(String[] args) {
 		String input1 = JOptionPane.showInputDialog("Enter a number");
 		String input2 = JOptionPane.showInputDialog("Enter another number");
@@ -14,18 +14,35 @@ public class Calculator {
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "divide", "multiply", "subtract", "add" }, null);
 
 		// 3) Call the correct method depending on what option the user chooses
-		
+if (task==3) {
+	
+}
 		// 4) Call the result() method and put the answer in a pop-up
 	}
-	
-	// 1) Make 4 static methods (add, subtract, multiply, divide)
-	//		They should each take 2 numbers as parameters
-	//		and return the answer.
-	 
-	
-	// 2) Make a static method called " result" that takes a number
-	//		as a parameter and returns the number in a string
-	//		EX: "Your answer is " + number;
 
+	// 1) Make 4 static methods (add, subtract, multiply, divide)
+	// They should each take 2 numbers as parameters
+	// and return the answer.
+	static int add(int num1, int num2) {
+int sum= num1+num2;
+return sum;
+	}
+
+	static void subtract() {
+
+	}
+
+	static void multiply() {
+
+	}
+
+	static void divide() {
+
+	}
+	// 2) Make a static method called " result" that takes a number
+	// as a parameter and returns the number in a string
+	// EX: "Your answer is " + number;
+static void result() {
 	
+}
 }
